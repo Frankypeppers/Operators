c3b4faf2ec4234bb539ea22ff2156b0f14b95eb4
Francisco Saroldi-Gonzalez <francisco.saroldi-gonzalez@mrjaffesclass.com>
2023-09-11 11:48:11 -0700

    started code

---
+++ Operators.java
@@ -0,0 +1,11 @@
+public class Operators 
+    {
+        public static void run() {
+            int i;
+            double f = 2.5;
+            boolean flag = true;
+            i = 10;
+            
+            
+        }
+    }
